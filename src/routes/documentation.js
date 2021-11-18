f48663e93c8cfd6e3e05600559d44d273c2406fa
cykic <cyb3rcykic@gmail.com>
2021-11-18 14:16:18 +0000

    Added Jest test

--- src/routes/documentation.js
+++ src/routes/documentation.js
@@ -1,13 +1,7 @@
 const express = require('express');
-const catchAsync = require('../error/catchAsync');
 
 const router = express.Router();
 
-router.all(
-  '/',
-  async (_, res, ) => {
-    res.redirect(process.env.DOCUMENTATION_URL);
-  }
-);
+router.get('/', (_, res) => res.redirect(process.env.DOCUMENTATION_URL));
 
 module.exports = router;
